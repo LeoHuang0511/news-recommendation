31786bfb0340326ed162593d8a73cc43b689b665
Leo Huang <leo95033@gmail.com>
2023-06-14 11:48:27 +0000

    add sgmoid for output

--- src/model/general/click_predictor/dot_product.py
+++ src/model/general/click_predictor/dot_product.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 
 
 class DotProductClickPredictor(torch.nn.Module):
@@ -16,4 +17,5 @@ class DotProductClickPredictor(torch.nn.Module):
         # batch_size, candidate_size
         probability = torch.bmm(candidate_news_vector,
                                 user_vector.unsqueeze(dim=-1)).squeeze(dim=-1)
+        probability = F.sigmoid(probability)
         return probability
